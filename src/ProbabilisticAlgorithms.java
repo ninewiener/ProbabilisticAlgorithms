1eb6a722ad84491116b8e7d4444de01cd917dc3c
ninewiener <janinebeckhaeuser@hotmail.com>
2017-06-14 14:40:15 +0200

    Init

---
+++ src/ProbabilisticAlgorithms.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Janine on 14.06.17.
+ */
+public class ProbabilisticAlgorithms {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    public void computePi(){
+
+    }
+
+}
